a0f57ac3fc5969f92cbe9eb757f8a41bc5857c8f
Glitch (strong-boulder-mouth) <none>
2020-07-17 00:24:40 +0000

    🌕🐑 Checkpoint ./server.js:12990682/1854

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const app = express();
-const Twitter = require("twitter-lite")
+const Twitter = require("twitter-lite");
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_API_KEY,
   consumer_secret: process.env.TWITTER_API_SECRET,
   access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
-
 
 app.use(express.static("public"));
 
-
-app.get("/", function(request, response) {
+app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
+
   (async () => {
-    const tweet = await client.post("statuses/update", {
-      status: "status test"
-    });
-  })()
+    try {
+      await client.post("statuses/update", {
+        status: "status test",
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  })();
 });
 
-
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });

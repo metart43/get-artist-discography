cfd439bab3083647963012baf0e82829a6d0e303
Glitch (strong-boulder-mouth) <none>
2020-07-17 19:55:53 +0000

    👠👋 Checkpoint ./getSongs.js:12990682/2535

--- getSongs.js
+++ getSongs.js
@@ -15,38 +15,40 @@ const getSongs = async () => {
       },
     });
     const { items } = response.data;
-    const radioheadAlbums = Object.values(
-      Object.fromEntries(
-        new Map(
-          items.map((album) => [
-            album.name,
-            {
-              id: album.id,
-              songs: (async () => {
-                try {
-                  const fetchedSongs = await axios({
-                    url: `https://api.spotify.com/v1/albums/${album.id}/tracks`,
-                    method: "GET",
-                    headers: {
-                      "Content-Type": "application/json",
-                      Authorization:
-                        "Bearer BQC3OcWrVc70k_TXlNCT6saF2aN3mQP2aDnOF3pTXwQGDA68qz9baeXFpUAXXeu7O696wxy0BH98FGSxDntrEtF1e4b9Zx-RbuJrbW5k8sksXaZgQ4kl36WL5fo3pYAQ54KGujxPqRCtceCRx8u_bdRB5sCF-gtNIy2nDnDLAS3Q4mhnhl-c-n4MuhbSfT1HZy5VatE3P5alh-RVCaSONS5sJQDf8tZPsvc7mwPp4tadnazPvWHOUXBeKW1rLOp44tfT0RMHudeL",
-                    },
-                  });
-                  const { items } = fetchedSongs.data;
-                  const songArray = items.map((song) => song.name);
-                  return songArray;
-                } catch (e) {
-                  console.log(e);
-                }
-              })(),
-            },
-          ])
-        )
+    const radioheadAlbums = Object.fromEntries(
+      new Map(
+        items.map((album) => [
+          album.name,
+          {
+            id: album.id,
+            songs: [],
+          },
+        ])
       )
     );
-    console.log(await radioheadAlbums);
 
+    console.log(radioheadAlbums);
+    const discography = Object.values(radioheadAlbums).map(({ id, songs }) =>
+      (async () => {
+        try {
+          const fetchedSongs = await axios({
+            url: `https://api.spotify.com/v1/albums/${id}/tracks`,
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization:
+                "Bearer BQC3OcWrVc70k_TXlNCT6saF2aN3mQP2aDnOF3pTXwQGDA68qz9baeXFpUAXXeu7O696wxy0BH98FGSxDntrEtF1e4b9Zx-RbuJrbW5k8sksXaZgQ4kl36WL5fo3pYAQ54KGujxPqRCtceCRx8u_bdRB5sCF-gtNIy2nDnDLAS3Q4mhnhl-c-n4MuhbSfT1HZy5VatE3P5alh-RVCaSONS5sJQDf8tZPsvc7mwPp4tadnazPvWHOUXBeKW1rLOp44tfT0RMHudeL",
+            },
+          });
+          const { items } = fetchedSongs.data;
+          items.forEach((song) => songs.push(song.name));
+        } catch (e) {
+          console.log(e);
+        }
+      })()
+    );
+    console.log(discography);
+    return discography;
     // return radioheadAlbums;
   } catch (e) {
     console.log(e);

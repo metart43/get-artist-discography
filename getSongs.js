09c5f80876f2c1dd03dcc6e8eb026bc060cfafde
Glitch (strong-boulder-mouth) <none>
2020-07-20 20:21:44 +0000

    🎐🐥 Checkpoint ./getSongs.js:12990682/1106

--- getSongs.js
+++ getSongs.js
@@ -1,6 +1,23 @@
 const axios = require("axios");
 const fs = require("fs");
 
+const getToken = async () => {
+  try {
+    const response = await axios({
+      url: `https://accounts.spotify.com/api/token`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `BASIC ${process.env.SPOTIFY_CLIENT_ID}`,
+      },
+    });
+    console.log(response);
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
 const matchSongs = async (id) => {
   try {
     const response = await axios({
@@ -8,6 +25,7 @@ const matchSongs = async (id) => {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
+        "Grant-type": "client_credentials",
         Authorization: `Bearer ${process.env.SPOTIFY_TOKEN}`,
       },
     });
@@ -22,25 +40,27 @@ const matchSongs = async (id) => {
 
 const getAlbums = async () => {
   try {
-    const response = await axios({
-      url:
-        "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb/albums?limit=40",
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.SPOTIFY_TOKEN}`,
-      },
-    });
-    const { items } = response.data;
-    const radioheadAlbums = await Promise.all(
-      items.map(async (album) => ({
-        [album.name]: {
-          id: album.id,
-          songs: await matchSongs(album.id),
-        },
-      }))
-    );
-    return radioheadAlbums;
+    const token = await getToken();
+    return null;
+    // const response = await axios({
+    //   url:
+    //     "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb/albums?limit=40",
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: `Bearer ${process.env.SPOTIFY_TOKEN}`,
+    //   },
+    // });
+    // const { items } = response.data;
+    // const radioheadAlbums = await Promise.all(
+    //   items.map(async (album) => ({
+    //     [album.name]: {
+    //       id: album.id,
+    //       songs: await matchSongs(album.id),
+    //     },
+    //   }))
+    // );
+    // return radioheadAlbums;
   } catch (e) {
     console.log(e);
   }
